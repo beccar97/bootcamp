1dd104354ec9d491a56a5c8b89a80ec5aa9ab5df
Becky Carter <becky.carter@softwire.com>
2019-11-19 16:03:42 +0000

    Part 2

--- FizzBuzz/src/com/company/Main.java
+++ FizzBuzz/src/com/company/Main.java
@@ -1,24 +1,53 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Main {
 
     public static void main(String[] args) {
-        for( int i = 0; i <= 200; i++) {
-            String output = "";
+        for( int i = 1; i <= 260; i++) {
+            LinkedList<String> outputWords = new LinkedList<String>();
 
             if (i % 11 == 0) {
-                // Don't need to check division by 3, 5, 7
-                if (i % 13 == 0) output = "FezzBong";
-                else output = "Fezz";
+                outputWords.add("Bong");
             } else {
-                if (i % 3 == 0) output += "Fizz";
-                if (i % 13 == 0) output += "Fezz";
-                if (i % 5 == 0) output += "Buzz";
-                if (i % 7 == 0) output += "Bang";
+                if (i % 3 == 0){
+                    outputWords.add("Fizz");
+                }
+                if (i % 5 == 0){
+                    outputWords.add("Buzz");
+                }
+                if (i % 7 == 0) {
+                    outputWords.add("Bang");
+                }
             }
 
-            if(!output.equals("")) System.out.println(output);
-            else System.out.println(i);
+            if (i % 13 == 0) {
+                if (outputWords.contains("Fizz")) {
+                    outputWords.add(1, "Fezz");
+                } else {
+                    outputWords.addFirst("Fezz");
+                }
+            }
+
+            if (outputWords.isEmpty()) {
+                System.out.println(i);
+            } else {
+                StringBuilder output = new StringBuilder();
+                ListIterator outputIterator = outputWords.listIterator();
+                if (i % 17 == 0) {
+                    while (outputIterator.hasNext()){
+                        output.insert(0, outputIterator.next());
+                    }
+                } else {
+                    while (outputIterator.hasNext()) {
+                        output.append(outputIterator.next());
+                    }
+                }
+                System.out.println(output);
+            }
+
         }
     }
 }
