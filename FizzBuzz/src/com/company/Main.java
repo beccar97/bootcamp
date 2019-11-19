4b8073600733ea2a4348343a40cfdd8e39618b29
Becky Carter <becky.carter@softwire.com>
2019-11-19 15:03:58 +0000

    Move FizzBuzz into own folder

--- FizzBuzz/src/com/company/Main.java
+++ FizzBuzz/src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    for (int i = 0; i <= 100; i++) {
+        for( int i = 0; i <= 100; i++) {
             String output = "";
             if (i % 3 == 0) output += "Fizz";
             if (i % 5 == 0) output += "Buzz";
 
-            if (!output.equals("")) System.out.println(output);
+            if(!output.equals("")) System.out.println(output);
             else System.out.println(i);
         }
     }
