6f80a0ba0f04da530e7ed037dd8cbb103e56aac1
Becky Carter <becky.carter@softwire.com>
2019-11-19 14:57:07 +0000

    Basic FizzBuzz

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for (int i = 0; i <= 100; i++) {
+            String output = "";
+            if (i % 3 == 0) output += "Fizz";
+            if (i % 5 == 0) output += "Buzz";
+
+            if (!output.equals("")) System.out.println(output);
+            else System.out.println(i);
+        }
+    }
+}
