f89c0cc8573867b6537ce9aecb0486c22aa2d76d
Becky Carter <becky.carter@softwire.com>
2019-11-20 13:41:52 +0000

    Log the 10 most common domains

--- Email Extraction/src/com/company/Main.java
+++ Email Extraction/src/com/company/Main.java
@@ -19,25 +19,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String fileContent = readFile();
 
-        Pattern emailPattern = Pattern.compile("\\b[\\w.'_%+-]+@(([\\w-]+\\.)+[\\w-]+)\\b");
-        Matcher emailMatcher = emailPattern.matcher(fileContent);
-
-        HashMap<String, Integer> emailDomains = new HashMap<String, Integer>();
-
-        while (emailMatcher.find()) {
-            String domain = emailMatcher.group(1);
-            emailDomains.put(domain, emailDomains.getOrDefault(domain, 0) + 1);
-        };
-
-        Map<String, Integer> sorted = emailDomains
-                .entrySet()
-                .stream()
-                .sorted(Collections.reverseOrder(comparingByValue()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-
-        System.out.println(sorted);
-
-
+        logCommonDomains(10, fileContent);
     }
 
     static String readFile() throws IOException {
@@ -59,5 +41,36 @@ public class Main {
         }
 
         return counter;
+    }
+
+    static Map<String, Integer> countDomains(String fileContent) {
+        Pattern emailPattern = Pattern.compile("\\b[\\w.'_%+-]+@(([\\w-]+\\.)+[\\w-]+)\\b");
+        Matcher emailMatcher = emailPattern.matcher(fileContent);
+
+        HashMap<String, Integer> emailDomains = new HashMap<String, Integer>();
+
+        while (emailMatcher.find()) {
+            String domain = emailMatcher.group(1);
+            emailDomains.put(domain, emailDomains.getOrDefault(domain, 0) + 1);
+        };
+
+        Map<String, Integer> sorted = emailDomains
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(comparingByValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+
+        return sorted;
+    }
+
+    static void logCommonDomains(int n, String fileContent) {
+        Map<String, Integer> domains = countDomains(fileContent);
+        String[] domainArray = domains.keySet().toArray(new String[domains.keySet().size()]);
+
+        System.out.println(String.format("The %d most common domains are:\n", n));
+
+        for (int i = 0; i < n && i < domainArray.length; i++) {
+            System.out.println(String.format("%s: %d occurrences", domainArray[i], domains.get(domainArray[i])));
+        }
     }
 }
