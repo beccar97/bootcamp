f26fd55aaa1f21ce6617d3929f2e1edb01384875
Becky Carter <becky.carter@softwire.com>
2019-11-20 10:56:58 +0000

    Count softwire emails using regex

--- Email Extraction/src/com/company/Main.java
+++ Email Extraction/src/com/company/Main.java
@@ -4,26 +4,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String fileContent = readFile();
-        int counter = 0;
-        for (int i = 0; i <= fileContent.length() - 13; i++) {
-            String substring = fileContent.substring(i, i + 13);
-            if (substring.equals("@softwire.com")) {
-                counter++;
-            }
-        }
-        System.out.println(counter);
+        int swEmailCount = countSwEMails(fileContent);
+
+        System.out.println(swEmailCount);
+
     }
 
     static String readFile() throws IOException {
-        Path filePath = Paths.get("C:/", "Work", "Training", "Email Extraction", "sample.txt");
+        Path filePath = Paths.get("sample.txt");
 
         String content = Files.readString(filePath);
 
         return content;
+    }
+
+    static int countSwEMails(String fileContent) {
+        Pattern swEmailPattern = Pattern.compile("[\\w.'_%+-]+@softwire\\.com");
+        Matcher swMatcher = swEmailPattern.matcher(fileContent);
+
+        int counter = 0;
+
+        while (swMatcher.find()) {
+            counter++;
+        }
+
+        return counter;
     }
 }
