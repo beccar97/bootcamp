1153a4288efab7c4ee6a0b64d69dbc8c2f2c5990
Becky Carter <becky.carter@softwire.com>
2020-01-16 15:37:27 +0000

    Strings: refactor

--- Strings/src/main/java/com/company/Main.java
+++ Strings/src/main/java/com/company/Main.java
@@ -1,77 +1,11 @@
 package main.java.com.company;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 
 public class Main {
-    private static int dateWidth = 11;
-    private static int titleWidth = 29;
-    private static int authorWidth = 21;
-    private static String formatString = ("| %-" + dateWidth + "s | %" + titleWidth + "s | %-" + authorWidth + "s |");
-    private static String bodyFormatString = ("| %-" + dateWidth + "s | %" + titleWidth + "s | %" + authorWidth + "s |");
 
     public static void main(String[] args) throws IOException {
-        System.out.println(processFile("simpleData.csv"));
-    }
-
-    public static String processFile(String fileName) throws IOException {
-        Path filePath = Paths.get(String.format("src/main/resources/%s", fileName));
-        BufferedReader fileBufferedReader = new BufferedReader(new FileReader(String.valueOf(filePath)));
-        StringBuilder output = new StringBuilder();
-
-        fileBufferedReader.readLine();
-        output.append(getTableHeader());
-
-        String line;
-        while ((line = fileBufferedReader.readLine()) != null) {
-            output.append("\n");
-            output.append(processEntry(line));
-        }
-        return output.toString();
-    }
-
-    private static String getTableHeader () {
-        String headers = String.format(formatString, "Pub Date", "Title", "Authors");
-        String divider = "\n|"
-                + String.join("", Collections.nCopies(headers.length() -2, "="))
-                + "|";
-        return headers + divider;
-    }
-
-    private static String processEntry(String line) {
-        String[] lineContents = line.split(",");
-        String dateString = processDate(lineContents[0]);
-        String titleString = processTitle(lineContents[1]);
-        String authorString = processAuthors(lineContents[2]);
-
-        return String.format(bodyFormatString, dateString, titleString, authorString);
-    }
-
-    private static String processDate(String dateInput) {
-        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
-        LocalDate publishDate = LocalDate.parse(dateInput, inputFormatter);
-
-        return publishDate.format(outputFormatter);
-    }
-
-    private static String processTitle(String titleInput) {
-        return shorten(titleInput, 29);
-    }
-
-    private static String processAuthors(String authorInput) {
-        return shorten(authorInput, 21);
-    }
-
-    private static String shorten(String string, int length) {
-        return string.length() <= length ? string
-                : string.substring(0,length - 3) + "...";
+        System.out.println(BookLister.processFile("simpleData.csv"));
     }
 }
 
--- Strings/src/test/java/com/company/BookListerTest.java
+++ Strings/src/test/java/com/company/BookListerTest.java
@@ -1,17 +1,17 @@
 package test.java.com.company;
-import main.java.com.company.Main;
+import main.java.com.company.BookLister;
 
-public class Test {
+public class BookListerTest {
     public static void main(String[] args) throws Exception {
-        givesCorrectOutput();
+        processFile_givesCorrectOutput();
     }
-    public static void givesCorrectOutput() throws Exception {
+    public static void processFile_givesCorrectOutput() throws Exception {
         String expectedOutput = "| Pub Date    |                         Title | Authors               |\n" +
                 "|=====================================================================|\n" +
                 "| 29 Jul 1954 |             Lord of the Rings | John Ronald Reuel ... |\n" +
                 "| 01 Aug 1996 |             A Game of Thrones | George Raymond Martin |\n" +
                 "| 21 Jun 2003 | Harry Potter and the Order... |        Joanne Rowling |";
-        String processedOutput = Main.processFile("simpleData.csv");
+        String processedOutput = BookLister.processFile("simpleData.csv");
         if (!processedOutput.equals(expectedOutput)) throw new Exception("Output is not equal to expected output");
         else System.out.println("Passed: givesCorrectOutput");
     }
